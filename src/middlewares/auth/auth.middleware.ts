77ad5fa39645c76e9a2f60534dfeec98e38c46a9
LEON <christopherleon237@gmail.com>
2024-11-14 14:11:16 +0100

    updated middleware

--- src/middlewares/auth/auth.middleware.ts
+++ src/middlewares/auth/auth.middleware.ts
@@ -3,6 +3,9 @@ import { getErrorMessage } from '../error_handlers/global-handler';
 import { AuthErrors } from '../../constants/errors.constants';
 import { StatusCodes } from 'http-status-codes';
 import { ROLES } from '../../constants/values.constants';
+import Access from '../../models/access.models';
+import { Op } from 'sequelize';
+import { isProductionEnv } from '../../utilities/guards';
 
 
 declare module 'express' {
@@ -11,7 +14,7 @@ declare module 'express' {
   }
 }
 
-export const authProtect = (req: Request, res: Response, next: NextFunction) => {
+export const authProtect = async (req: Request, res: Response, next: NextFunction) => {
   const modes: string[] = ['live', 'test']
   const user: string[] = ['admn', 'usr']
   const accessKey: string = req.headers['x-api-access-key'] as string;
@@ -24,14 +27,29 @@ export const authProtect = (req: Request, res: Response, next: NextFunction) => 
     return getErrorMessage(next, AuthErrors.NOT_AUTHORIZED, StatusCodes.UNAUTHORIZED);
   if (!modes.includes(accessKey.split('_')[1])) return getErrorMessage(next, AuthErrors.NOT_AUTHORIZED, StatusCodes.UNAUTHORIZED);
   if (!user.includes(accessKey.split('_')[2])) return getErrorMessage(next, AuthErrors.NOT_AUTHORIZED, StatusCodes.UNAUTHORIZED);
-  let role: string
-  if (accessKey.split('_')[2] === 'admn') role = ROLES.admin
-  if (accessKey.split('_')[2] === 'usr') role = ROLES.user
-  req.user = { accessKey, role }
-  next()
-} 
-
-
+  if (isProductionEnv && accessKey.split('_')[1] === 'test') return getErrorMessage(next, AuthErrors.NOT_AUTHORIZED, StatusCodes.UNAUTHORIZED);
+  let validKey: Access
+  if (accessKey.split('_')[2] === 'admin') {
+    validKey = await Access.findOne({
+      where: {
+        [Op.or]: { live_admin_key: accessKey, test_admin_key: accessKey }
+      }
+    })
+    if (accessKey.split('_')[2] === 'user') {
+      validKey = await Access.findOne({
+        where: {
+          [Op.or]: { live_user_key: accessKey, test_user_key: accessKey }
+        }
+      })
+    }
+    if (!validKey) return getErrorMessage(next, AuthErrors.NOT_AUTHORIZED, StatusCodes.UNAUTHORIZED);
+    let role: string
+    if (accessKey.split('_')[2] === 'admn') role = ROLES.admin
+    if (accessKey.split('_')[2] === 'usr') role = ROLES.user
+    req.user = { accessKey, role }
+    next()
+  }
+}
 export const authRestrictTo = (roles: string[]) => {
   return async (req: Request, res: Response, next: NextFunction) => {
     if (!roles.includes(req.user.role))
