03707e17f3b3962edc4b50f24e52cbb2e43e4045
LEON <christopherleon237@gmail.com>
2024-11-14 11:52:51 +0100

    refactored camopaign servive

--- src/services/campaign.ts
+++ src/services/campaign.ts
@@ -11,7 +11,7 @@ import { getUpdatedFields } from "../utilities/global.utilities";
 export default class CampaignService {
     public getCampaignById = (async (campaignId: string, next: NextFunction) => {
         try {
-            const data = await Campaign.findOne({ where: { campaignId }, attributes:{exclude: ['id']}  })
+            const data = await Campaign.findOne({ where: { campaignId }, attributes: { exclude: ['id'] } })
             if (!data) return getErrorMessage(next, CampaignErrors.NOT_FOUND, StatusCodes.NOT_FOUND)
             return data
         } catch (error) {
@@ -21,7 +21,7 @@ export default class CampaignService {
 
     public getCampaigns = (async (paginate: PaginationObject, next: NextFunction) => {
         try {
-            const responseData = await Campaign.findAndCountAll({ ...paginate, attributes:{exclude: ['id']} });
+            const responseData = await Campaign.findAndCountAll({ ...paginate, attributes: { exclude: ['id'] } });
             const { rows, count } = responseData
             const data = { count, data: rows }
             return data
@@ -45,7 +45,7 @@ export default class CampaignService {
             if (!campaign) return getErrorMessage(next, CampaignErrors.NOT_FOUND, StatusCodes.NOT_FOUND)
             const updatedFields = getUpdatedFields(campaign.toJSON(), data);
             if (Object.keys(updatedFields).length > 0) {
-                const campaign=await Campaign.update(updatedFields, {where:{campaignId}, returning:true})
+                const campaign = await Campaign.update(updatedFields, { where: { campaignId }, returning: true })
                 return campaign[1][0]
             }
             return campaign
@@ -68,8 +68,7 @@ export default class CampaignService {
         try {
             const campaign = await this.getCampaignById(campaignId, next)
             if (!campaign) return getErrorMessage(next, CampaignErrors.NOT_FOUND, StatusCodes.NOT_FOUND)
-            // const metrics = await getMetrics(campaignId)
-            const data = { campaign, metrics: "checked" }
+            const data = { campaign, metrics: [] }
             return data
         } catch (error) {
             return getErrorMessage(next, "An error occurred", StatusCodes.INTERNAL_SERVER_ERROR)
